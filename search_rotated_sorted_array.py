b7ce3996ad3c8eb7a012e40bcc5df1927ae7c463
Rifat Al Mahfuz <38405888+Rifat12@users.noreply.github.com>
2021-03-28 01:07:16 +0600

    Added new version

--- search_rotated_sorted_array.py
+++ search_rotated_sorted_array.py
@@ -42,3 +42,39 @@ class Solution:
                     right = mid - 1
         # cannot find the target value
         return -1
+    
+        # My Version
+        '''
+        if mid > leftmost element:
+        we're in the leftside of the pivot which means (leftmost....mid) is a uninterrupted sorted subarray.
+            if the target is beween leftmost and mid we apply binary search there and discard anything right of mid.
+            Otherwise the target is right of the mid so we apply binary search right of the mid and discard anything left of mid
+        
+        else:
+        we're in the right side of the pivot. 
+            if mid...target...rightmost then we discard anything left of mid apply binary search there.
+            otherwise the target is at left of mid so we apply binary search there and discard anything on the right.
+        
+        '''
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid =(left+right)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] >= nums[left]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+                    
+
+    
